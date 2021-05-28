267a434e8f84cf4d32c91a6ce5abe72b6b7e3fd6
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-05-28 11:46:27 +0300

    Deleted unused controller

--- src/main/java/com/shop/controller/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.shop.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/order")
-public class OrderController {
-}
